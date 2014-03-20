c9756be2754a5c6aae0757fe93bc1a5675222b3a
Shane G <gautreau@uoguelph.ca>
2014-03-20 18:31:17 -0400

    Added the live badge and fixed resource path issue

--- VrPlayer/Views/VrGui/VrGui.cs
+++ VrPlayer/Views/VrGui/VrGui.cs
@@ -24,11 +24,7 @@ namespace VrPlayer.Views.VrGui
         private static double CANVAS_HEIGHT     = 400;          // GUI menu base panel height
         private static double CELL_PADDING      = 8;            // Pseudo-cell padding for the content grid\
 
-#if DEBUG // Base address for the resource files is different in debug mode then it is for release 
-        private static string BASE_DIR = @"C:\Users\41X\Documents\41X\VrPlayer";
-#else
-        private static string BASE_DIR = System.AppDomain.CurrentDomain.BaseDirectory;
-#endif
+        private static string BASE_DIR = "pack://application:,,,/Medias/VrGui/";
 
 
         private Point _mouseXY = new Point(0, 0);
@@ -50,6 +46,7 @@ namespace VrPlayer.Views.VrGui
         {
             // Main canvas
             Canvas c = new Canvas();
+            c.VerticalAlignment = VerticalAlignment.Bottom;
             c.Width = CANVAS_WIDTH;
             c.Height = CANVAS_HEIGHT;
             c.Background = new SolidColorBrush(Color.FromArgb(0x00, 0x00, 0x00, 0x00));
@@ -59,6 +56,7 @@ namespace VrPlayer.Views.VrGui
             StackPanel mainPanel = new StackPanel();
             mainPanel.Width = CANVAS_WIDTH;
             mainPanel.Height = CANVAS_HEIGHT;
+            mainPanel.VerticalAlignment = VerticalAlignment.Bottom;
 
             //Title block for the menu
             TextBlock title = new TextBlock();
@@ -67,14 +65,18 @@ namespace VrPlayer.Views.VrGui
             title.TextAlignment = TextAlignment.Center;
             title.Text = "MAIN MENU";
             title.Margin = new Thickness(2);
-            title.Foreground = Brushes.Black;
-            title.Background = new SolidColorBrush(Color.FromRgb(0xEC, 0xEC, 0xEC));
+            //title.Foreground = Brushes.Black;
+            //title.Background = new SolidColorBrush(Color.FromRgb(0xEC, 0xEC, 0xEC));
+            title.Foreground = new SolidColorBrush(Color.FromRgb(0xEC, 0xEC, 0xEC));
 
             mainPanel.Children.Add(title);
 
             // Grid Panel for the content selection and content navigation
             Grid content = createContentGrid(CANVAS_WIDTH, CANVAS_HEIGHT/2);
             mainPanel.Children.Add(content);
+
+            Grid progress = createProgressGrid(CANVAS_WIDTH/1.5, CANVAS_HEIGHT / 4);
+            mainPanel.Children.Add(progress);
 
             c.Children.Add(mainPanel);
 
@@ -86,7 +88,6 @@ namespace VrPlayer.Views.VrGui
             Canvas.SetTop(m, _mouseXY.Y);
             Canvas.SetLeft(m, _mouseXY.X);
             m.ClipToBounds = true;
-            m.MouseUp += MouseUpHandler;
 
             c.Children.Add(m);
 
@@ -94,17 +95,17 @@ namespace VrPlayer.Views.VrGui
         }
 
         /// <summary>
-        /// Creates a 2x5 grid layout to display the content (videos/pictures) available for viewing</summary>
+        /// Creates a 2x5 grid layout</summary>
         /// <param name="width">Preferred Grid Panel width</param>
         /// <param name="height">Preferred Grid Panel height</param>
-        private Grid createContentGrid(double width, double height)
+        private Grid createGrid(double width, double height)
         {
-            Grid content = new Grid();
-            content.Width = width;
-            content.Height = height;
-            content.HorizontalAlignment = HorizontalAlignment.Center;
-            content.VerticalAlignment = VerticalAlignment.Center;
-            content.ShowGridLines = false;
+            Grid grid = new Grid();
+            grid.Width = width;
+            grid.Height = height;
+            grid.HorizontalAlignment = HorizontalAlignment.Center;
+            grid.VerticalAlignment = VerticalAlignment.Center;
+            grid.ShowGridLines = false;
 
             // Define the columns and rows for the content selection.
             // This GUI is using 2 rows and 5 columns ( 3 for content and 2 for navigation)
@@ -113,56 +114,124 @@ namespace VrPlayer.Views.VrGui
             ColumnDefinition colDef3 = new ColumnDefinition();
             ColumnDefinition colDef4 = new ColumnDefinition();
             ColumnDefinition colDef5 = new ColumnDefinition();
-            content.ColumnDefinitions.Add(colDef1);
-            content.ColumnDefinitions.Add(colDef2);
-            content.ColumnDefinitions.Add(colDef3);
-            content.ColumnDefinitions.Add(colDef4);
-            content.ColumnDefinitions.Add(colDef5);
+            grid.ColumnDefinitions.Add(colDef1);
+            grid.ColumnDefinitions.Add(colDef2);
+            grid.ColumnDefinitions.Add(colDef3);
+            grid.ColumnDefinitions.Add(colDef4);
+            grid.ColumnDefinitions.Add(colDef5);
 
             RowDefinition rowDef1 = new RowDefinition();
             RowDefinition rowDef2 = new RowDefinition();
-            content.RowDefinitions.Add(rowDef1);
-            content.RowDefinitions.Add(rowDef2);
+            grid.RowDefinitions.Add(rowDef1);
+            grid.RowDefinitions.Add(rowDef2);
+
+            return grid;
+
+        }
+
+        /// <summary>
+        /// Creates a 2x5 grid layout to display the progress bar</summary>
+        /// <param name="width">Preferred Grid Panel width</param>
+        /// <param name="height">Preferred Grid Panel height</param>
+        private Grid createProgressGrid(double width, double height)
+        {
+            Grid progress = createGrid(width, height);
+            progress.VerticalAlignment = VerticalAlignment.Bottom;
+            
+            Rectangle bar = new Rectangle();
+            bar.Fill = new SolidColorBrush(Color.FromRgb(0xEC, 0xEC, 0xEC));
+            bar.Height = (height/4) - (CELL_PADDING*2);
+            Grid.SetColumnSpan(bar, 3);
+            Grid.SetColumn(bar, 1);
+            Grid.SetRow(bar, 0);
+            progress.Children.Add(bar);
+
+            //Title block for elapsed time on the video
+            TextBlock elapsed = new TextBlock();
+            FontSizeConverter fsc = new FontSizeConverter();
+            elapsed.FontSize = (double)fsc.ConvertFrom("20pt");
+            elapsed.TextAlignment = TextAlignment.Center;
+            elapsed.Text = "-:--";
+            elapsed.Margin = new Thickness(2);
+            elapsed.Foreground = new SolidColorBrush(Color.FromRgb(0xEC, 0xEC, 0xEC));
+            Grid.SetColumn(elapsed, 0);
+            Grid.SetRow(elapsed, 0);
+            progress.Children.Add(elapsed);
+
+            //Title block for the full length of the video
+            TextBlock length = new TextBlock();
+            length.FontSize = (double)fsc.ConvertFrom("20pt");
+            length.TextAlignment = TextAlignment.Center;
+            length.Text = "-:--";
+            length.Margin = new Thickness(2);
+            length.Foreground = new SolidColorBrush(Color.FromRgb(0xEC, 0xEC, 0xEC));
+            Grid.SetColumn(length, 4);
+            Grid.SetRow(length, 0);
+            progress.Children.Add(length);
+
+            return progress;
+        }
+
+        /// <summary>
+        /// Creates a 2x5 grid layout to display the content (videos/pictures) available for viewing</summary>
+        /// <param name="width">Preferred Grid Panel width</param>
+        /// <param name="height">Preferred Grid Panel height</param>
+        private Grid createContentGrid(double width, double height)
+        {
+            Grid content = createGrid(width, height);
 
             // Get all the icons for the content
             // TODO: Differentiate between a video and a photo for the proper icon. Just using video for now
-            Image icon1 = createImageIcon(CANVAS_WIDTH / 5 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\video_icon.png");
+            Image icon1 = createImageIcon("video_icon.png", CANVAS_WIDTH / 5 - CELL_PADDING);
+            icon1.Width = (CANVAS_WIDTH / 5) - CELL_PADDING;
             Grid.SetColumn(icon1, 1);
             Grid.SetRow(icon1, 0);
             content.Children.Add(icon1);
+            // TODO: This will require some sort of "isLive" logic
+            //if (isLive(null)) addLiveBadge(content, 0, 3);
 
-            Image icon2 = createImageIcon(CANVAS_WIDTH / 5 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\video_icon.png");
+            Image icon2 = createImageIcon("video_icon.png", CANVAS_WIDTH / 5 - CELL_PADDING);
             Grid.SetColumn(icon2, 2);
             Grid.SetRow(icon2, 0);
             content.Children.Add(icon2);
-            
-            Image icon3 = createImageIcon(CANVAS_WIDTH / 5 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\video_icon.png");
+            // TODO: This will require some sort of "isLive" logic
+            //if (isLive(null)) addLiveBadge(content, 0, 3);
+
+            Image icon3 = createImageIcon("video_icon.png", CANVAS_WIDTH / 5 - CELL_PADDING);
             Grid.SetColumn(icon3, 3);
             Grid.SetRow(icon3, 0);
             content.Children.Add(icon3);
+            // TODO: This will require some sort of "isLive" logic
+            if (isLive(null)) addLiveBadge(content, 0, 3);
 
-            Image icon4 = createImageIcon(CANVAS_WIDTH / 5 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\video_icon.png");
+            Image icon4 = createImageIcon("video_icon.png", CANVAS_WIDTH / 5 - CELL_PADDING);
             Grid.SetColumn(icon4, 1);
             Grid.SetRow(icon4, 1);
             content.Children.Add(icon4);
+            // TODO: This will require some sort of "isLive" logic
+            //if (isLive(null)) addLiveBadge(content, 0, 3);
 
-            Image icon5 = createImageIcon(CANVAS_WIDTH / 5 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\video_icon.png");
+            Image icon5 = createImageIcon("video_icon.png", CANVAS_WIDTH / 5 - CELL_PADDING);
             Grid.SetColumn(icon5, 2);
             Grid.SetRow(icon5, 1);
             content.Children.Add(icon5);
+            // TODO: This will require some sort of "isLive" logic
+            addLiveBadge(content, 1, 2);
 
-            Image icon6 = createImageIcon(CANVAS_WIDTH / 5 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\video_icon.png");
+            Image icon6 = createImageIcon("video_icon.png", CANVAS_WIDTH / 5 - CELL_PADDING);
             Grid.SetColumn(icon6, 3);
             Grid.SetRow(icon6, 1);
             content.Children.Add(icon6);
+            // TODO: This will require some sort of "isLive" logic
+            //if (isLive(null)) addLiveBadge(content, 0, 3);
 
             // These will be the navigation buttons for the content grid
-            Image prevIcon = createImageIcon(CANVAS_WIDTH / 10 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\prev_icon.png");
+            Image prevIcon = createImageIcon("prev_icon.png", CANVAS_WIDTH / 10 - CELL_PADDING);
             Grid.SetColumn(prevIcon, 0);
             Grid.SetRowSpan(prevIcon, 2);
             content.Children.Add(prevIcon);
 
-            Image nextIcon = createImageIcon(CANVAS_WIDTH / 10 - CELL_PADDING, BASE_DIR + @"\Medias\VrGui\next_icon.png");
+            Image nextIcon = createImageIcon("next_icon.png", CANVAS_WIDTH / 10 - CELL_PADDING);
             Grid.SetColumn(nextIcon, 5);
             Grid.SetRowSpan(nextIcon, 2);
             content.Children.Add(nextIcon);
@@ -170,21 +239,32 @@ namespace VrPlayer.Views.VrGui
             return content;
         }
 
+        private void addLiveBadge(Grid content, int row, int col)
+        {
+            Image liveBadge = createImageIcon("live_icon.png", 25);
+            liveBadge.HorizontalAlignment = HorizontalAlignment.Left;
+            liveBadge.VerticalAlignment = VerticalAlignment.Top;
+            liveBadge.Margin = new Thickness(10, 30, 0, 0);
+
+            Grid.SetRow(liveBadge, row);
+            Grid.SetColumn(liveBadge, col);
+            content.Children.Add(liveBadge);
+        }
+
         /// <summary>
         /// Creates an image of specified width from the URI provided</summary>
         /// <param name="width">Preferred image width</param>
         /// <param name="uri">File location for the image resource</param>
-        private Image createImageIcon(double width, string uri)
+        private Image createImageIcon(String filename, double width)
         {
             Image img = new Image();
             img.Width = width;
-
-            // Create source
-            BitmapImage bmpImg = new BitmapImage();
+            
+            BitmapImage bmi = new BitmapImage();
 
             // BitmapImage.UriSource must be in a BeginInit/EndInit block
-            bmpImg.BeginInit();
-            bmpImg.UriSource = new Uri(uri);
+            bmi.BeginInit();
+            bmi.UriSource = new Uri(BASE_DIR + filename, UriKind.Absolute);
 
             // To save significant application memory, set the DecodePixelWidth or   
             // DecodePixelHeight of the BitmapImage value of the image source to the desired  
@@ -193,12 +273,19 @@ namespace VrPlayer.Views.VrGui
             // the size that is displayed. 
             // Note: In order to preserve aspect ratio, set DecodePixelWidth 
             // or DecodePixelHeight but not both.
-            bmpImg.DecodePixelWidth = 200;
-            bmpImg.EndInit();
+            bmi.DecodePixelWidth = 200;
+            bmi.EndInit();
+
             //set image source
-            img.Source = bmpImg;
+            img.Source = bmi;
 
             return img;
+        }
+
+        private bool isLive(MediaElement media)
+        {
+            // TODO: implement logic to determine if media is a stream or a file
+            /*Stub*/return true;
         }
 
         // TODO: Implement some sort of handler for mouse clicks
